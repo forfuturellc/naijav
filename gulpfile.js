f7feacc94976a911a87a38464e528c28cffffd34
Gocho Mugo <mugo@forfuture.co.ke>
2015-01-06 20:57:11 +0300

    Task `jshint` for linting js files
    `gulp jshint` will lint all javascript files and report the errors.
    For this purpose, a new dependency, "gulp-jshint", has been added
    to "package.json".

--- gulpfile.js
+++ gulpfile.js
@@ -1,34 +1,25 @@
 var gulp = require('gulp');
 var gutil = require('gulp-util');
 var bower = require('bower');
-var concat = require('gulp-concat');
-var sass = require('gulp-sass');
-var minifyCss = require('gulp-minify-css');
-var rename = require('gulp-rename');
 var sh = require('shelljs');
 
+
+// gulp plugins
+var concat = require('gulp-concat');
+var jshint = require("gulp-jshint");
+var minifyCss = require('gulp-minify-css');
+var rename = require('gulp-rename');
+var sass = require('gulp-sass');
+
+
+// Paths used repeatedly
 var paths = {
+  js: ["www/js/*.js"],
   sass: ['./scss/**/*.scss']
 };
 
-gulp.task('default', ['sass']);
 
-gulp.task('sass', function(done) {
-  gulp.src('./scss/ionic.app.scss')
-    .pipe(sass())
-    .pipe(gulp.dest('./www/css/'))
-    .pipe(minifyCss({
-      keepSpecialComments: 0
-    }))
-    .pipe(rename({ extname: '.min.css' }))
-    .pipe(gulp.dest('./www/css/'))
-    .on('end', done);
-});
-
-gulp.task('watch', function() {
-  gulp.watch(paths.sass, ['sass']);
-});
-
+// task: installs gulp
 gulp.task('install', ['git-check'], function() {
   return bower.commands.install()
     .on('log', function(data) {
@@ -36,6 +27,8 @@ gulp.task('install', ['git-check'], function() {
     });
 });
 
+
+// task: checks if git is installed
 gulp.task('git-check', function(done) {
   if (!sh.which('git')) {
     console.log(
@@ -48,3 +41,35 @@ gulp.task('git-check', function(done) {
   }
   done();
 });
+
+
+// task: compiles sass into css
+gulp.task('sass', function(done) {
+  gulp.src('./scss/ionic.app.scss')
+    .pipe(sass())
+    .pipe(gulp.dest('./www/css/'))
+    .pipe(minifyCss({
+      keepSpecialComments: 0
+    }))
+    .pipe(rename({ extname: '.min.css' }))
+    .pipe(gulp.dest('./www/css/'))
+    .on('end', done);
+});
+
+
+// task: hints javascript files
+gulp.task("jshint", function() {
+  return gulp.src(paths.js)
+    .pipe(jshint())
+    .pipe(jshint.reporter("default"));
+});
+
+
+// task: watches changes
+gulp.task('watch', function() {
+  gulp.watch(paths.sass, ['sass']);
+});
+
+
+// task: the default task
+gulp.task('default', ['sass']);
