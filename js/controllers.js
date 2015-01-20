170bd4b2e74a678246ebe7bb3522cf4a283c6dcc
Gocho Mugo <mugo@forfuture.co.ke>
2015-01-21 00:51:40 +0300

    Implemented changing settings and sending feedback
    Users may send feedback and change settings from the user panel
    
    :sunglasses:

--- js/controllers.js
+++ js/controllers.js
@@ -4,6 +4,13 @@ var timeout = 5000;
 
 angular.module('Naijav.Site.Controllers', ["ngResource"])
 
+
+.config(['$httpProvider', function($httpProvider) {
+  $httpProvider.defaults.withCredentials = true;
+  //$httpProvider.defaults.headers.post['Content-Type'] = 'application/x-www-form-urlencoded;charset=utf-8';
+}])
+
+
 .controller('AccountCtrl', ['$scope', '$http', '$location', '$sce', function($scope, $http, $location, $sce) {
   // Scope variables
   $scope.memberCount =
@@ -62,7 +69,7 @@ angular.module('Naijav.Site.Controllers', ["ngResource"])
       password: $scope.loginPassword
     })
     .success(function(data, status, headers, config) {
-      $scope.alertMessage = $sce.trustAsHtml("<strong>Okey!</strong> You are logged in! Switching you to your settings.");
+      $scope.alertMessage = $sce.trustAsHtml("<strong>Okey!</strong> You are logged in! Switching you to your user panel.");
       $scope.alertClass = "alert-success";
       setTimeout(function() {
         $location.path("/user");
@@ -84,17 +91,14 @@ angular.module('Naijav.Site.Controllers', ["ngResource"])
 
 .controller('UserCtrl', ['$scope', '$sce', '$location', '$http', function($scope, $sce, $location, $http) {
   // $scope variables
+  $scope.settings = {};
   $scope.alertMessage =
   $scope.alertClass =
-  $scope.emailUpdates =
+  $scope.settings.emailUpdates =
   $scope.feedback = null;
 
-  // retrieving user data
-  $http.get(backendUrl + "/members/settings")
-  .success(function(data) {
-    $scope.emailUpdates = !!data.email_updates;
-  })
-  .error(function(data, status, headers) {
+  function errorHandler(data, status, headers, config) {
+    console.log(data);
     if (status === 401 || status === 403) {
       $scope.alertMessage = $sce.trustAsHtml("<strong>Hey!</strong> You not logged in! Redirecting to Login!");
       setTimeout(function() {
@@ -102,8 +106,58 @@ angular.module('Naijav.Site.Controllers', ["ngResource"])
         $scope.$apply();
       }, timeout);
     } else {
-      $scope.alertMessage = $sce.trustAsHtml("<strong>Hey!</strong> Finding it hard to retrieve data!");
+      $scope.alertMessage = $sce.trustAsHtml("<strong>Hey!</strong> Finding it hard to exchange data with server!");
     }
     $scope.alertClass = "alert-danger";
+  }
+
+  // retrieving user data
+  $http.get(backendUrl + "/members/settings")
+  .success(function(data) {
+    $scope.emailUpdates = !!data.email_updates;
+    $('input[ng-checked="settings.emailUpdates"]').bootstrapSwitch('state', !!data.email_updates);
+  })
+  .error(errorHandler);
+
+  // Updates Settings in remote server
+  function updateRemote() {
+    $http.post(backendUrl + "/members/settings", {
+      email_updates: String($scope.settings.emailUpdates)
+    })
+    .success(function(data) {
+      $scope.alertMessage = $sce.trustAsHtml("<i class='fa fa-smile-o'></i> Settings saved!");
+      $scope.alertClass = "alert-success alert-flush";
+      setTimeout(function() {
+        $scope.alertMessage = null;
+        $scope.$apply();
+      }, timeout / 2);
+    })
+    .error(errorHandler);
+  }
+
+  // Watching on the settings to keep updating remote server
+  $scope.$watch("settings", function(updatedSettings) {
+    updateRemote();
+  }, true);
+  // Still watching settings, particularly for the checkboxes
+  $('input[type="checkbox"]').on('switchChange.bootstrapSwitch', function(event, state) {
+    updateRemote();
   });
+
+  // Sending user feedback
+  $scope.sendFeedback = function sendFeedback() {
+    $http.post(backendUrl + "/members/feedback", {
+      feedback: String($scope.feedback)
+    })
+    .success(function(data) {
+      $scope.alertMessage = $sce.trustAsHtml("<i class='fa fa-smile-o'></i> Feedback received!");
+      $scope.alertClass = "alert-success alert-flush";
+      setTimeout(function() {
+        $scope.alertMessage = null;
+        $scope.$apply();
+      }, timeout / 2);
+    })
+    .error(errorHandler);
+  };
+
 }])
